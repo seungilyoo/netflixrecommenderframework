3d62f5d54f694372bc49f60406ab406fc7520be9
Benjamin C Meyer <ben@meyerhome.net>
2006-11-24 23:41:00 +0000

    much cleaner

--- algorithms/pearson/main.cpp
+++ algorithms/pearson/main.cpp
@@ -28,8 +28,10 @@
 
 #include <probe.h>
 #include <user.h>
+
 #include <qdebug.h>
 #include <qvector.h>
+
 /*
 # Compute the Pearson correlation coefficient between two arrays of numbers:
 #
@@ -65,176 +67,89 @@ double pearsonCorrelationCoefficient(const QVector<int> &x, const QVector<int> &
     the average of all the votes for a movie and guess's that for any user.
 
 */
-class DoubleAverage : public Algorithm {
+bool first(const QPair<double,int> &a, const QPair<double,int> &b){ return a.first > b.first; };
+class Pearson : public Algorithm {
+
 public:
-    DoubleAverage(DataBase *db) : currentMovie(db), user(db), currentMovieAverage(-1) {
-        foo.fill(-1, db->totalVotes());
+    Pearson(DataBase *db) : currentMovie(db), user(db) {
+        currentUserVotes.fill(-1, db->totalVotes());
     }
-    Algorithm* create() { return new DoubleAverage(currentMovie.dataBase()); };
 
     void setMovie(int id)
     {
         currentMovie.setId(id);
-        currentMovieAverage = 0;
-        uint currentMovieVotes = currentMovie.votes();
-        for (uint i = 0; i < currentMovieVotes; ++i)
-            currentMovieAverage += currentMovie.score(i);
-        currentMovieAverage = currentMovieAverage / (double)(currentMovie.votes());
     }
+
 
     double determine(int userId)
     {
         user.setId(userId);
         movieUsers.resize(currentMovie.votes());
         User otherUser(currentMovie.dataBase());
-        int c = 0;
-        uint nextUser = 0;
 
-        uint currentMovieId = currentMovie.id();
+        // populate for quick lookup (usually faster then seenMovie)
+        int currentMovieId = currentMovie.id();
         for (int i = 0; i < user.votes(); ++i)
-            foo[user.movie(i)] = user.score(i);
+            currentUserVotes[user.movie(i)] = user.score(i);
 
         for (uint i = 0; i < currentMovie.votes(); ++i) {
-            nextUser = currentMovie.user(i);
-            if (nextUser == (uint)userId)
+            int nextUser = currentMovie.user(i);
+            if (nextUser == userId)
                 continue;
             otherUser.setId(nextUser);
-            c = 0;
+            int c = 0;
 
             int userVotes = otherUser.votes();
             if (x.size() < userVotes) {
                 x.resize(userVotes);
                 y.resize(userVotes);
             }
+
             for (int j = 0; j < userVotes; ++j) {
                 int movie = otherUser.movie(j);
                 if (movie == currentMovieId)
                     continue;
                 //int r = user.seenMovie(movie);
-                int r = foo.at(movie);
+                int r = currentUserVotes.at(movie);
                 if (r != -1) {
                     x[c] = (r);
                     y[c] = (otherUser.score(j));
                     ++c;
                 }
             }
-            movieUsers[i] = pearsonCorrelationCoefficient(x, y, c);
-        }
-
-        int biggest = 0;
-        double largest = -1.0;
-        int perfectusers[5] = {0,0,0,0,0};
-        double top = 1.1;
-        int bounce = 0;
-        do {
-            bounce = 0;
-            top -= 0.1;
-            for (int i = 0; i < movieUsers.count(); ++i) {
-                int cs = currentMovie.score(i);
-                if (movieUsers.at(i) >= top) {
-                    perfectusers[cs - 1]++;
-                    ++bounce;
-                }
-                if (largest < movieUsers.at(i)) {
-                    largest = movieUsers.at(i);
-                    biggest = i;
-                }
+            if (c > ((user.votes() / 100) * 70)) {
+                double result = pearsonCorrelationCoefficient(x, y, c);
+                movieUsers[i] = QPair<double, int>(result, currentMovie.score(i));
             }
-        } while (bounce <= 5 && top > 0);
-
-        int match = 0;
-        for (int i = 0; i < 5; ++i) {
-            if (perfectusers[i] > perfectusers[match])
-                match = i;
         }
-        for (int i = 0; i < user.votes(); ++i)
-            foo[user.movie(i)] = -1;
 
-
-        qDebug() << perfectusers[0] << perfectusers[1] << perfectusers[2] << perfectusers[3] << perfectusers[4] << "pick" << match << "votes:" << currentMovie.votes() << top;
-        if (match > -1 && perfectusers[match] != 0) {
-            if (perfectusers[match + 1] == perfectusers[match])
-                return ((double)(match + 2 + match + 1)/2);
-            return (match + 1);
-
-        }
-        return currentMovie.score(biggest);
+        qSort(movieUsers.begin(), movieUsers.end(), first);
+        uint total = 0;
+        int i;
+        for (i = 0; i < 200 && i < movieUsers.count(); ++i)
+            total += movieUsers.at(i).second;
+        if (i == 0)
+            return 3;
+        return total/(double)i;
     }
 
-    QVector<int> foo;
+    QVector<int> currentUserVotes;
 
     QVector<int> x;
     QVector<int> y;
-    QVector<double> movieUsers;
+
+    QVector<QPair<double, int> > movieUsers;
 
     Movie currentMovie;
     User user;
-    double currentMovieAverage;
 };
-#include <qbitarray.h>
+
 int main(int , char **)
 {
     DataBase db;
     db.load();
-/*
-    User firstUser(&db, 6);
-
-    User user(firstUser);
-    Movie movie(&db, 0);
-    QBitArray otherUsers(db.totalUsers());
-    while (user.id() != db.lastUser()) {
-        *//*int c = 0;
-        for (int i = 0; i < user.votes(); ++i) {
-            movie.setId(user.movie(i));
-            for (uint j = 0; j < movie.votes(); ++j) {
-                otherUsers[db.mapUser(movie.user(j))] = true;
-            }
-
-        }
-        //qSort(otherUsers.begin(), otherUsers.end());
-        qDebug() << otherUsers.count() << db.totalUsers() << c;
-        return 0;
-        *//*
-        User otherUser(firstUser);
-        while (otherUser.id() != db.lastUser()) {
-            otherUser.next();
-        }
-        return 0;
-        user.next();
-        //qDebug() << user.id();
-    }
-    qDebug() << user.id();
-    return 0;
-    /*
-    user.setId(userId);
-        QVector<double> movieUsers(currentMovie.votes());
-        QVector<int> x(32);
-        QVector<int> y(32);
-        for (uint i = 0; i < currentMovie.votes(); ++i) {
-            uint nextUser = currentMovie.user(i);
-            if (nextUser == (uint)userId)
-                continue;
-            User otherUser(currentMovie.dataBase(), currentMovie.user(i));
-            int c = 0;
-            for (int j = 0; j < user.votes(); ++j) {
-                int movie = user.movie(j);
-                int r = otherUser.seenMovie(movie);
-                if (r != -1) {
-                    if (c == x.size()) {
-                        x.resize(x.size() * 2 + 1);
-                        y.resize(y.size() * 2 + 1);
-                    }
-                    x[c] = (user.score(j));
-                    y[c] = (r);
-                    ++c;
-                }
-            }
-            movieUsers[i] = pearsonCorrelationCoefficient(x, y, c);
-        }
-*/
-    
     Probe probe(&db);
-    DoubleAverage bf(&db);
-    return probe.runProbe(&bf);
+    Pearson algorithm(&db);
+    return probe.runProbe(&algorithm);
 }
 
